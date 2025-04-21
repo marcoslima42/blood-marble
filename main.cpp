c2a4b8bf2b9f31113967521adfb8bf23f3a2790a
Marcos Lima <araujolima728@gmail.com>
2025-04-21 01:13:05 -0300

    Update: link com a lib opengl32
    Teste da api

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <cassert>
+
+
 using namespace std;
 
 const int Width = 800;
@@ -9,15 +12,40 @@ const int Height = 600;
 int main() {
 	// imprimi se for false o retorno ao iniciar o glfw
 	// inicializa a bilbioteca GLFW
-	assert(glfwInit());
+	assert(glfwInit() == GLFW_TRUE);
 
 	// Criar uma janela
 	GLFWwindow* Window = glfwCreateWindow(Width, Height, "Blood Marble", nullptr, nullptr);
 	assert(Window);
+	// Ativa contexto criado na janela Window
+	glfwMakeContextCurrent(Window);
 
+	// Inicializa a lib GLEW
+	assert(glewInit() == GLEW_OK);
+
+	//verificar versão do OpenGL
+	GLint GLMajorVersion = 0;
+	GLint GLMinorVersion = 0;
+	glGetIntegerv(GL_MAJOR_VERSION, &GLMajorVersion);
+	glGetIntegerv(GL_MINOR_VERSION, &GLMinorVersion);
 	//Entra no loop de eventos da aplicacao
+	cout << "OpenGL Version  : " << GLMajorVersion << "." << GLMinorVersion << endl;
+	
+	// Usar o glGetString() para obter informações do driver que estamos usando
+	cout << "OpenGL Vendor   : " << glGetString(GL_VENDOR) << endl;
+	cout << "OpenGL Renderer : " << glGetString(GL_RENDERER) << endl;
+	cout << "OpenGL Version  : " << glGetString(GL_VERSION) << endl;
+	cout << "GLSL VERSION    : " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
+	
+
+	// Definir a cor de fundo 
+	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+	
 	while (!glfwWindowShouldClose(Window))
 	{
+		// limap o framebuffer.
+		glClear(GL_COLOR_BUFFER_BIT);
+		
 		// Processa todos os eventos da fila de eventos do GLFW
 		// Eventos podem ser eventos do teclado, mouse, gamepad
 		glfwPollEvents();
